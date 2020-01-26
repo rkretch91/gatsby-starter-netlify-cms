b9d6108cf84770d8d38ad448c670b8ef422c3749
Ryan Kretch <rkretch91@gmail.com>
2020-01-26 20:06:25 +0100

    cdefd

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -3,11 +3,10 @@ import Layout from '../../components/Layout'
 
 export default () => (
   <Layout>
-    <section className="section">
-      <div className="container">
+    <section className="section" style={{padding: '0px'}}>
+      <div className="backgroundimg">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Thank you, you sexy human!</h1>
         </div>
       </div>
     </section>
